2b320aac292395d1b51b85439a0b4a511c1ec797
jcaltamia <jayceealtamia@gmail.com>
2022-02-07 16:43:45 +0800

    js

---
+++ app/js/main.js
@@ -0,0 +1,81 @@
+const inputHero = document.querySelector('.hero__email');
+const inputCta = document.querySelector('.cta__email');
+const infoTextHero = document.querySelector('.hero__info-text');
+const forms = document.querySelectorAll('.email-form');
+const inputs = document.querySelectorAll('.email-form input');
+const infoTextCta = document.querySelector('.cta__info-text');
+
+const buttonHero = document.querySelector('.hero__button');
+const buttonCta = document.querySelector('.cta__button');
+
+buttonHero.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (validateEmail(inputHero.value)) {
+    infoTextHero.innerHTML = 'Please Check your Email';
+    infoTextHero.style.color = 'blue';
+     inputHero.value = '';
+  } else {
+    infoTextHero.innerHTML = 'Incorrect Email Format';
+    infoTextHero.style.color = 'red';
+  }
+});
+
+buttonCta.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (validateEmail(inputCta.value)) {
+    infoTextCta.innerHTML = 'Please Check your Email';
+    infoTextCta.style.color = 'blue';
+    inputCta.value= "";
+  } else {
+    infoTextCta.innerHTML = 'Incorrect Email Format';
+    infoTextCta.style.color = 'red';
+  }
+});
+
+// forms.forEach((form) => {
+//   form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     inputs.forEach((input) => {
+//       if (validateEmail(input.value)) {
+//         if (input.classList.contains('hero__email')) {
+//           infoTextHero.innerHTML = 'Please Check your Email';
+//           infoTextHero.style.color = 'black';
+//         } else {
+//           infoTextHero.innerHTML = 'Incorrect Email Format';
+//           infoTextHero.style.color = 'red';
+//         }
+//       }
+
+//       if (input.classList.contains('cta__email')) {
+//         if (validateEmail(input.value)) {
+//           infoTextCta.innerHTML = 'Please Check your Email';
+//           infoTextCta.style.color = 'black';
+//         } else {
+//           infoTextCta.innerHTML = 'Incorrect Email Format';
+//           infoTextCta.style.color = 'red';
+//         }
+//       }
+//     });
+
+// if (validateEmail(input.value)) {
+//   if (form.classList.contains('hero__form-container')) {
+//     infoTextHero.innerHTML = 'Please Check your Email';
+//     infoTextHero.style.color = 'black';
+//   } else {
+//     infoTextCta.innerHTML = 'Please Check your Email';
+//     infoTextCta.style.color = 'black';
+//   }
+// } else {
+//   infoTextHero.innerHTML = 'Incorrect Email Format';
+//   infoTextHero.style.color = 'red';
+//   infoTextCta.innerHTML = 'Incorrect Email Format';
+//   infoTextCta.style.color = 'red';
+// }
+//   });
+// });
+
+function validateEmail(email) {
+  let emailCheck =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailCheck.test(String(email).toLowerCase());
+}
